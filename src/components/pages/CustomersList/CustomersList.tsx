12d7b87b3b8fa56a98157a174c8fedb1acac0540
nik95ta <nikta.hashemi1995@yahoo.com>
2023-12-04 15:05:19 +0330

    Filter customers by industry

--- src/components/pages/CustomersList/CustomersList.tsx
+++ src/components/pages/CustomersList/CustomersList.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
+import { CustomerInterface } from '../../../interfaces';
 import { CustomersListHeader, CustomersTable } from '../../../components';
 import styles from './CustomersList.module.css';
-import filteredCustomers from '../../../customers.json';
+import customers from '../../../customers.json';
 
 const CustomersList: React.FC = () => {
-  const [, setFilter] = useState<string>('');
+  const [filter, setFilter] = useState<string>('');
   const [, setSelectedCustomerId] = useState<string | null>(null);
+
+  const filteredCustomers = useMemo(
+    () => customers?.filter((customer: CustomerInterface) => (filter ? customer.industry === filter : true)) || [],
+    [customers, filter],
+  );
+
+  const uniqueIndustries = useMemo(
+    () => [...new Set(customers.map((customer: CustomerInterface) => customer.industry))],
+    [customers],
+  );
 
   const handleEditCustomer = (id: string) => {
     setSelectedCustomerId(id);
@@ -23,7 +34,7 @@ const CustomersList: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <CustomersListHeader onAddCustomer={handleAddCustomer} onChangeFilter={setFilter} industries={[]} />
+      <CustomersListHeader onAddCustomer={handleAddCustomer} onChangeFilter={setFilter} industries={uniqueIndustries} />
       <CustomersTable customers={filteredCustomers} onEdit={handleEditCustomer} onDelete={handleDeleteCustomer} />
     </div>
   );
